c068772aa6d2fc965f2314adcb35bdb7b107ca8a
paralunstarcutter <92927633+paralunstarcutter@users.noreply.github.com>
2021-11-08 15:49:35 +0200

    Update class.py

--- class.py
+++ class.py
@@ -4,7 +4,6 @@ class Rectangle:
         self.b = b
 
     def calc_perimeter(self):
-        # Бажано, щоб назва функції відповідала на питання, що робить ця функція.
         return 2 * (self.a + self.b)
 
     def calc_surface(self):
@@ -13,18 +12,4 @@ class Rectangle:
 
 class Square(Rectangle):
     def square_check(self):
-        # Невеличкий lifehack, якщо в тебе функція перевіряє True або False, можеш просто зробити так
         return self.a == self.b
-
-# Перед выдправкою коду на перевірку, краще видаляй закоментовані частинки коду нижче. Буде виглядати "професійніше" ) 
-
-#x = Square(2,10)
-#y = Square(10,10)
-#print(x.p())
-#print(x.s())
-#print(x.sq())
-#print(y.p())
-#print(y.s())
-#print(y.sq())
-
-# Добрий день, це Репіна Софія. Надалі буду (намагатися) відсилати роботи через GitHub
